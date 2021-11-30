fe47ab77fb099fa9f95534bbc86e71b9a3f9cb87
David Hérault <dherault@gmail.com>
2021-11-30 16:30:30 +0100

    Iterate

--- front/src/scenes/Home/index.js
+++ front/src/scenes/Home/index.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { ethers } from 'ethers'
 
 import { Button, Container, FormControl, FormHelperText, MenuItem, Select, TextField, Tooltip, Typography } from '@mui/material'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 
+const defaultBlockchain = '0x4'
 const blockchainToMetadata = {
   '0x1': {
     name: 'Ethereum',
@@ -10,14 +12,30 @@ const blockchainToMetadata = {
     logo: '/images/ethereum-logo-small.png',
     description: 'Ethereum is the largest blockchain supporting NFTs. It is the default blockchain we recommend for this application.',
   },
+  '0x4': {
+    name: 'Ethereum Rinkeby Test Network',
+    currency: 'ETH',
+    logo: '/images/ethereum-logo-small.png',
+    description: 'The Rinkeby Test Network should be used for testing purposes only.',
+  },
 }
 
 function Home() {
   const [loading, setLoading] = useState(false)
   const [content, setContent] = useState('')
-  const [blockchain, setBlockchain] = useState('0x1')
+  const [blockchain, setBlockchain] = useState(defaultBlockchain)
   const [accounts, setAccounts] = useState([])
   const [selectedAccount, setSelectedAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [balance, setBalance] = useState(null)
+
+  async function handleBlockchainChange(event) {
+    const nextBlockchain = event.target.value
+
+    setBlockchain(nextBlockchain)
+
+    await handleBlockchainOrAccountChange(nextBlockchain)
+  }
 
   async function handleMetamaskClick() {
     const hasMetamask = window.ethereum?.isMetaMask
@@ -31,35 +49,49 @@ function Home() {
     setLoading(true)
 
     const accountAddresses = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const nextSelectedAccountAddress = accountAddresses[0]
 
-    await window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0x1' }] })
+    setAccounts(accountAddresses)
+    setSelectedAccount(nextSelectedAccountAddress)
+
+    await handleBlockchainOrAccountChange(null, nextSelectedAccountAddress)
 
     setLoading(false)
-    setAccounts(accountAddresses)
-    setSelectedAccount(accountAddresses[0])
 
-    console.log('accountAddresses', accountAddresses)
+    // console.log('x', accountAddresses, balance)
+  }
+
+  async function handleBlockchainOrAccountChange(nextBlockchain, nextSelectedAccount) {
+    await window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: nextBlockchain || blockchain }] })
+
+    const nextProvider = new ethers.providers.Web3Provider(window.ethereum)
+    const nextBalance = await nextProvider.getBalance(nextSelectedAccount || selectedAccount)
+
+    setProvider(nextProvider)
+    setBalance(ethers.utils.formatEther(nextBalance))
   }
 
   function renderSignInButtons() {
     return (
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        startIcon={(
-          <img
-            src="/images/metamask-logo.png"
-            alt="Metamask"
-            width={32}
-            className="mr-1"
-          />
-        )}
-        onClick={handleMetamaskClick}
-        disabled={loading}
-      >
-        Sign in with Metamask
-      </Button>
+      <div className="ml-1 x41">
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={(
+            <img
+              src="/images/metamask-logo.png"
+              alt="Metamask"
+              width={32}
+              className="mr-1"
+            />
+          )}
+          onClick={handleMetamaskClick}
+          disabled={loading}
+        >
+          Sign in with Metamask
+        </Button>
+      </div>
     )
   }
 
@@ -82,19 +114,11 @@ function Home() {
               </MenuItem>
             ))}
           </Select>
-          <FormHelperText className="x41">
-            Carefully select which account the NFT will belong to.
-            <div className="pt-0h">
-              <Tooltip title="Pick one with enough funds, or if uncertain the first one.">
-                <HelpOutlineIcon
-                  className="ml-1"
-                  fontSize="tiny"
-                />
-              </Tooltip>
-            </div>
+          <FormHelperText>
+            Carefully select which account the NFT will belong to. If uncertain pick the first one.
           </FormHelperText>
         </FormControl>
-        <div className="ml-2 pt-1h x41">
+        {/* <div className="ml-2 pt-1h x41">
           <img
             src={blockchainToMetadata[blockchain].logo}
             alt={blockchainToMetadata[blockchain].name}
@@ -106,7 +130,7 @@ function Home() {
           >
             {blockchainToMetadata[blockchain].currency} 12.0000000987645638
           </Typography>
-        </div>
+        </div> */}
       </div>
     )
   }
@@ -116,6 +140,9 @@ function Home() {
       maxWidth="md"
       className="mt-4"
     >
+      {/*
+        LOGO AND TITLE
+      */}
       <div className="x41">
         <img
           src="/images/logo.png"
@@ -135,6 +162,9 @@ function Home() {
           Welcome! Here you can create a <strong>Non-Fungible Token</strong> on the Ethereum blockchains. This process is called <strong>minting</strong>.
         </Typography>
       </section>
+      {/*
+        CONTENT
+      */}
       <section className="mt-6 x11">
         <Typography
           variant="h6"
@@ -157,36 +187,9 @@ function Home() {
           </FormHelperText>
         </FormControl>
       </section>
-      <section className="mt-2 x11">
-        <Typography
-          variant="h6"
-          color="gradient1"
-          className="pt-1h"
-          style={{ width: 128 }}
-        >
-          Blockchain:
-        </Typography>
-        <FormControl className="ml-1 flex-grow">
-          <Select
-            value={blockchain}
-            onChange={e => setBlockchain(e.target.value)}
-            color="primary"
-            fullWidth
-          >
-            {Object.keys(blockchainToMetadata).map(blockchainId => (
-              <MenuItem
-                key={blockchainId}
-                value={blockchainId}
-              >
-                {blockchainToMetadata[blockchainId].name}
-              </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText>
-            {blockchainToMetadata[blockchain].description}
-          </FormHelperText>
-        </FormControl>
-      </section>
+      {/*
+        ACCOUNT
+      */}
       <section className="mt-2 x11">
         <Typography
           variant="h6"
@@ -198,6 +201,69 @@ function Home() {
         </Typography>
         {selectedAccount ? renderAccountSelection() : renderSignInButtons()}
       </section>
+      {/*
+        BLOCKCHAIN
+      */}
+      {!!selectedAccount && (
+        <section className="mt-2 x11">
+          <Typography
+            variant="h6"
+            color="gradient1"
+            className="pt-1h"
+            style={{ width: 128 }}
+          >
+            Blockchain:
+          </Typography>
+          <FormControl className="ml-1 flex-grow">
+            <Select
+              value={blockchain}
+              onChange={handleBlockchainChange}
+              color="primary"
+              fullWidth
+            >
+              {Object.keys(blockchainToMetadata).map(blockchainId => (
+                <MenuItem
+                  key={blockchainId}
+                  value={blockchainId}
+                >
+                  {blockchainToMetadata[blockchainId].name}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>
+              {blockchainToMetadata[blockchain].description}
+            </FormHelperText>
+          </FormControl>
+        </section>
+      )}
+      {/*
+        BALANCE
+      */}
+      {!!selectedAccount && (
+        <section className="mt-2 x11">
+          <Typography
+            variant="h6"
+            color="gradient2"
+            className="pt-1h"
+            style={{ width: 128 }}
+          >
+            Balance:
+          </Typography>
+          <div className="ml-2 pt-1h x41">
+            <img
+              src={blockchainToMetadata[blockchain].logo}
+              alt={blockchainToMetadata[blockchain].name}
+              width={16}
+            />
+            <Typography
+              variant="h6"
+              className="ml-1"
+            >
+              {blockchainToMetadata[blockchain].currency} {balance}
+            </Typography>
+          </div>
+        </section>
+      )}
     </Container>
   )
 }
---
+++ services/ethereum/scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require('hardhat')
+
+async function main() {
+  const Contract = await ethers.getContractFactory('NFTCreatorV1')
+  const contract = await Contract.deploy()
+
+  console.log('Contract deployed to address:', contract.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
